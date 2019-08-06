88537251af8a36f15de2062b80ccf9e4b2e369ef
bonfy <foreverbonfy@163.com>
2019-08-06 15:54:10 +0800

    fix test

--- file_test.go
+++ file_test.go
@@ -2,10 +2,10 @@ package btool
 
 import "testing"
 
-func TestGetExt(t *testing.T) {
+func TestGetFileExt(t *testing.T) {
 	in := "./hello/readme.md"
 	expected := ".md"
-	actual := GetExt(in)
+	actual := GetFileExt(in)
 	if actual != expected {
 		t.Errorf("GetExt(%s) = %s; expected %s", in, actual, expected)
 	}
